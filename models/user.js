ea1614c5c132323485a45d39da6fe1d165fa6b9d
Jess Stayton <jessica.stayton@gmail.com>
2022-06-29 14:06:07 -0600

    committing completed models

--- models/user.js
+++ models/user.js
@@ -1,31 +1,80 @@
 /** User class for message.ly */
 
-
+const { BCRYPT_WORK_FACTOR, DB_URI } = require('../config');
+const bcrypt = require('bcrypt');
+const db = require('../db');
+const ExpressError = require('../expressError');
 
 /** User of the site. */
 
 class User {
-
-  /** register new user -- returns
+	/** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+	static async register({ username, password, first_name, last_name, phone }) {
+		const hashedPwd = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+		const results = await db.query(
+			`INSERT INTO users (
+        username, 
+        password, 
+        first_name, 
+        last_name, 
+        phone, 
+        join_at, 
+        last_login_at)
+        VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
+        RETURNING username, password, first_name, last_name, phone, join_at, last_login_at`,
+			[ username, hashedPwd, first_name, last_name, phone ]
+		);
+		if (results.rows.length === 0) {
+			throw new ExpressError(`No such user: ${username}`, 404);
+		}
+		return results.rows[0];
+	}
 
-  /** Authenticate: is this username/password valid? Returns boolean. */
+	/** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+	static async authenticate(username, password) {
+		const results = await db.query(
+			`SELECT password
+      FROM users
+      WHERE username = $1`,
+			[ username ]
+		);
+		if (results.rows.length === 0) {
+			throw new ExpressError(`No such user: ${username}`, 404);
+		}
+		const user = results.rows[0];
+		return user && (await bcrypt.compare(password, user.password));
+	}
 
-  /** Update last_login_at for user */
+	/** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+	static async updateLoginTimestamp(username) {
+		const result = await db.query(
+			`UPDATE users SET last_login_at=current_timestamp WHERE username=$1 RETURNING last_login_at`,
+			[ username ]
+		);
 
-  /** All: basic info on all users:
+		if (!result.rows[0]) {
+			throw new ExpressError(`No such user: ${username}`, 404);
+		}
+	}
+
+	/** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+	static async all() {
+		const results = await db.query(
+			`SELECT username, first_name, last_name, phone
+      FROM users
+      ORDER BY username`
+		);
+		return results.rows;
+	}
 
-  /** Get: get user by username
+	/** Get: get user by username
    *
    * returns {username,
    *          first_name,
@@ -34,9 +83,20 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+	static async get(username) {
+		const results = await db.query(
+			`SELECT username, first_name, last_name, phone, join_at, last_login_at
+      FROM users
+      WHERE username = $1`,
+			[ username ]
+		);
+		if (results.rows.length === 0) {
+			throw new ExpressError(`No such user: ${username}`, 404);
+		}
+		return results.rows[0];
+	}
 
-  /** Return messages from this user.
+	/** Return messages from this user.
    *
    * [{id, to_user, body, sent_at, read_at}]
    *
@@ -44,9 +104,42 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+	static async messagesFrom(username) {
+		const results = await db.query(
+			`SELECT 
+      m.id, 
+      m.to_username, 
+      m.body, 
+      m.sent_at, 
+      m.read_at,
+      u.username, 
+      u.first_name, 
+      u.last_name,
+      u.phone
+      FROM messages AS m
+      JOIN users AS u ON m.to_username = u.username
+      WHERE m.from_username = $1
+      `,
+			[ username ]
+		);
+		if (results.rows.length === 0) {
+			throw new ExpressError(`No such user: ${username}`, 404);
+		}
+		return results.rows.map((m) => ({
+			id: m.id,
+			to_user: {
+				username: m.username,
+				first_name: m.first_name,
+				last_name: m.last_name,
+				phone: m.phone
+			},
+			body: m.body,
+			sent_at: m.sent_at,
+			read_at: m.read_at
+		}));
+	}
 
-  /** Return messages to this user.
+	/** Return messages to this user.
    *
    * [{id, from_user, body, sent_at, read_at}]
    *
@@ -54,8 +147,40 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+	static async messagesTo(username) {
+		const results = await db.query(
+			`SELECT 
+      m.id, 
+      m.from_username, 
+      m.body, 
+      m.sent_at, 
+      m.read_at,
+      u.username, 
+      u.first_name, 
+      u.last_name,
+      u.phone
+      FROM messages AS m
+      JOIN users AS u ON m.from_username = u.username
+      WHERE m.to_username = $1
+      `,
+			[ username ]
+		);
+		if (results.rows.length === 0) {
+			throw new ExpressError(`No such user: ${username}`, 404);
+		}
+		return results.rows.map((m) => ({
+			id: m.id,
+			from_user: {
+				username: m.username,
+				first_name: m.first_name,
+				phone: m.phone,
+				last_name: m.last_name
+			},
+			body: m.body,
+			sent_at: m.sent_at,
+			read_at: m.read_at
+		}));
+	}
 }
-
 
 module.exports = User;
